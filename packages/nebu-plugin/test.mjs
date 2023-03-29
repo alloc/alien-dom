cc1d06ff9179eff36132a6217a967514a5db8686
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-03-28 22:34:43 -0400

    chore: update nebu-plugin test

--- packages/nebu-plugin/test.mjs
+++ packages/nebu-plugin/test.mjs
@@ -1,6 +1,6 @@
 import { nebu } from 'nebu'
 import { green } from 'kleur/colors'
-import plugin from './src/plugin'
+import plugin from './dist/plugin.js'
 
 const result = nebu.process(
   `import {selfUpdating} from 'alien-dom'
@@ -15,7 +15,18 @@ function Foo() {
     return <div>{button}</div>
   })
 
-  return <div>{button}<Bar/></div>
+  return <div>
+    {button}
+    <SomeContext.Provider value={1}>
+      <Test test={<Test />} />
+    </SomeContext.Provider>
+    <Bar placeholder={<Placeholder />}>
+      <Test>
+        <div><Test /></div>
+        {condition ? <Test /> : null}
+      </Test>
+    </Bar>
+  </div>
 }
 `,
   {
