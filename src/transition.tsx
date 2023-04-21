4e578aadfd6f0af742d2e5c93791efb206c68673
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-04-20 20:26:14 -0400

    feat: add `Transition` component
    This component is useful for animating between two elements. The `id` prop identifies the `children` prop, so a transition can be gracefully interrupted during animation (rather than abruptly mounting a new element) if the `id` prop is changed to match a leaving element.
    
    This component is also a good example of using the new <ManualUpdates> element and `updateNode` function. Leaving elements are wrapped in an absolute-positioned container, so as to not affect the layout of the current children. Normal use of alien-dom prohibits moving children of a self-updating component to a new parent element, because alien-dom uses element keys to match elements only in the same parent. This is a problem for the Transition component, but the ManualUpdates API is an easy workaround. Yet, the Transition component still wants children to be update-able from the parent component, so it calls `updateNode` to handle that.

---
+++ src/transition.tsx
@@ -0,0 +1,206 @@
+import { selfUpdating } from './selfUpdating'
+import { SpringAnimation, animate } from './animate'
+import { JSX } from './types/jsx'
+import { useState } from './hooks/useState'
+import { fromElementProp } from './fromElementProp'
+import { $ } from './selectors'
+import { isElement } from './isElement'
+import { useEffect } from './hooks/useEffect'
+import { updateNode } from './updateElement'
+import { CSSProperties } from './types/dom'
+import { AnyElement } from './internal/types'
+import { kFragmentNodeType, kElementNodeType } from './internal/constants'
+import { ManualUpdates } from './manualUpdates'
+
+type Falsy = false | null | undefined
+
+/** The style applied to the container that wraps leaving elements. */
+const leaveStyle: CSSProperties = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  pointerEvents: 'none',
+}
+
+export const Transition = selfUpdating(function <T>(props: {
+  id: T
+  enter: (
+    id: T,
+    element: JSX.Element,
+    initial: boolean,
+    relatedId: T | undefined
+  ) => SpringAnimation<JSX.Element> | SpringAnimation<JSX.Element>[] | Falsy
+  leave: (
+    id: T,
+    element: JSX.Element,
+    relatedId: T | undefined
+  ) => SpringAnimation<JSX.Element> | SpringAnimation<JSX.Element>[] | Falsy
+  children: JSX.ElementsProp
+}) {
+  const state = useState(initialState)
+
+  const previousId = props.id !== state.currentId ? state.currentId : undefined
+  const leavingElements = Array.from(
+    state.children,
+    ([id, element]) => id !== props.id && id !== previousId && element
+  ).filter(Boolean)
+
+  let children = fromElementProp(props.children)
+  let newLeavingElements: AnyElement[] | undefined
+  let newEnteredElements: AnyElement[] | undefined
+  let initial = false
+
+  if (previousId !== undefined || state.children.size === 0) {
+    if (previousId !== undefined) {
+      // We can assume the previous elements are an array, because we
+      // don't wrap entered elements in a container.
+      newLeavingElements = state.elements.get(previousId) as AnyElement[]
+
+      if (newLeavingElements.length) {
+        const container = <div key={Math.random()} style={leaveStyle} />
+        container.append(...newLeavingElements)
+        state.elements.set(previousId, container)
+        leavingElements.push(container)
+      }
+    }
+
+    const previousChildren = state.children.get(props.id)
+    children = Array.isArray(children) ? <>{children}</> : children
+
+    if (children) {
+      if (previousChildren) {
+        const previousElements = state.elements.get(props.id)!
+        if (Array.isArray(previousElements)) {
+          // If an array is found, then no elements were rendered before,
+          // so this an empty array was stored (and never wrapped).
+          newEnteredElements = previousElements
+        } else {
+          // Leaving elements are wrapped in an absolute-positioned
+          // container, which we want to remove now that the elements are
+          // re-entering.
+          newEnteredElements = $(previousElements)
+            .unwrap()
+            .filter(child => isElement(child)) as JSX.Element[]
+        }
+        updateNode(previousChildren, children)
+        children = previousChildren
+      } else {
+        initial = true
+        newEnteredElements = toElements(children)
+        state.children.set(props.id, children)
+      }
+      state.elements.set(props.id, newEnteredElements)
+    } else {
+      state.children.delete(props.id)
+      state.elements.delete(props.id)
+    }
+  }
+
+  useEffect(() => {
+    state.currentId = props.id
+
+    newEnteredElements?.forEach(element => {
+      const transition = props.enter(
+        props.id,
+        element as JSX.Element,
+        initial,
+        previousId
+      )
+      if (transition) {
+        animate(element as JSX.Element, transition)
+      }
+    })
+
+    if (newLeavingElements) {
+      if (newLeavingElements.length) {
+        const container = state.children.get(previousId) as JSX.Element
+
+        let leavingCount = newLeavingElements.length
+        const onTransitionEnd = () => {
+          if (--leavingCount === 0) {
+            container.remove()
+            state.children.delete(previousId)
+          }
+        }
+
+        newLeavingElements.forEach(element => {
+          let transition = props.leave(
+            previousId,
+            element as JSX.Element,
+            props.id
+          )
+          if (transition) {
+            if (!Array.isArray(transition)) {
+              const { onRest } = transition
+              transition.onRest = (...args) => {
+                onRest?.(...args)
+                onTransitionEnd()
+              }
+            } else if (transition.length) {
+              // Assume the last animation is the last to finish.
+              const { onRest } = transition[transition.length - 1]
+              transition[transition.length - 1].onRest = (...args) => {
+                onRest?.(...args)
+                onTransitionEnd()
+              }
+            } else {
+              transition = false
+            }
+          }
+          if (transition) {
+            animate(element as JSX.Element, transition)
+          } else {
+            onTransitionEnd()
+          }
+        })
+      } else {
+        state.children.delete(previousId)
+      }
+    }
+  }, [props.id])
+
+  return (
+    <ManualUpdates
+      // Since this component isn't transformed due to being in
+      // node_modules, we have to manually wrap the children with a thunk
+      // to ensure the ManualUpdates effect is applied.
+      children={() => {
+        return [leavingElements, children]
+      }}
+    />
+  )
+})
+
+interface TransitionState {
+  currentId: any
+  /** The most recent `children` prop for each `id` prop. */
+  children: Map<any, AnyElement>
+  /** The animated elements of each `id` prop. */
+  elements: Map<any, AnyElement | AnyElement[]>
+}
+
+function initialState(): TransitionState {
+  return {
+    currentId: undefined,
+    children: new Map(),
+    elements: new Map(),
+  }
+}
+
+function toElements(element: JSX.ElementOption) {
+  if (!element) {
+    return []
+  }
+  if (element.nodeType === kFragmentNodeType) {
+    const children: AnyElement[] = []
+    element.childNodes.forEach(child => {
+      if (child.nodeType === kElementNodeType) {
+        children.push(child as AnyElement)
+      }
+    })
+    return children
+  }
+  return [element]
+}
