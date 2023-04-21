3e9211fb4c8adc397535c975eaf9508b1e8ae379
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-04-20 21:26:34 -0400

    fix: mount detection of a component's root fragment
    If a component returns a fragment as its root node, we have to use its first child node (which is always a comment node created by alien-dom) to know if the fragment has been mounted.

--- src/jsx-dom/jsx-runtime.ts
+++ src/jsx-dom/jsx-runtime.ts
@@ -272,11 +272,17 @@ function appendChild(child: JSX.Children, parent: Node, key?: string) {
     if (kAlienElementTags.in(child)) {
       const tags = kAlienElementTags(child)!
       queueMicrotask(() => {
-        if (!(child as ChildNode).isConnected) {
+        const rootNode =
+          (child as ChildNode).nodeType === kFragmentNodeType
+            ? kAlienFragment(child)![0]
+            : (child as ChildNode)
+
+        if (!rootNode.isConnected) {
           // The element hasn't mounted yet, so we'll have to rely on
           // the component to set an `onMount` listener.
           return
         }
+
         const components = Array.from(tags.values())
         if (components[0].hooks) {
           for (const component of components) {
