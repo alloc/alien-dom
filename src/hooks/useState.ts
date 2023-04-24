bb543c876d471964eada45efdd2c08ee4dc989c3
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-04-24 16:21:48 -0400

    feat(useState): support class constructors as the `init` argument

--- src/hooks/useState.ts
+++ src/hooks/useState.ts
@@ -3,8 +3,25 @@ import { currentComponent } from '../global'
 export function useState<State extends object, Params extends any[]>(
   init: (...params: Params) => State,
   ...params: Params
-): State {
+): State
+
+export function useState<State extends object, Params extends any[]>(
+  init: new (...args: Params) => State,
+  ...params: Params
+): State
+
+export function useState(init: StateInitializer, ...params: any[]) {
   const component = currentComponent.get()!
   const index = component.memoryIndex++
-  return (component.memory[index] ||= init(...params))
+  return (component.memory[index] ||= isClass(init)
+    ? new init(...params)
+    : init(...params))
+}
+
+type StateInitializer =
+  | ((...params: any[]) => object)
+  | (new (...args: any[]) => object)
+
+function isClass(arg: StateInitializer): arg is new (...args: any[]) => any {
+  return arg.toString().startsWith('class ')
 }
