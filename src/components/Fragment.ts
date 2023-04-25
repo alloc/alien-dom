3c3068e826c51bb8eee73d9253bc565b181a7199
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-04-25 17:24:25 -0400

    feat: short-circuit `Fragment` constructor when `props.children` is a fragment
    No point in wrapping a fragment with another fragment.

--- src/components/Fragment.ts
+++ src/components/Fragment.ts
@@ -1,8 +1,13 @@
 import type { JSX } from '../types'
-import { appendChild } from '../jsx-dom/appendChild'
+import { kFragmentNodeType } from '../internal/constants'
 import { markPureComponent } from '../functions/markPureComponent'
+import { appendChild } from '../jsx-dom/appendChild'
+import { isElement } from '../jsx-dom/util'
 
 export function Fragment(props: { children: JSX.Children }): JSX.Element {
+  if (isElement(props.children, kFragmentNodeType)) {
+    return props.children as any
+  }
   const fragment = document.createDocumentFragment()
   appendChild(props.children, fragment)
   return fragment as any
