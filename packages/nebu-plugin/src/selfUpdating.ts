89afa79de5aa0c7302fe2d1325c194b9d9e41e5f
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-05-04 18:17:23 -0400

    fix(compiler): stop wrapping objects/arrays declared as variables during render
    For this to work, the compiler needs to guarantee the object/array is not being mutated during render, which requires more work than I want to put in currently.

--- packages/nebu-plugin/src/selfUpdating.ts
+++ packages/nebu-plugin/src/selfUpdating.ts
@@ -215,8 +215,6 @@ export default function (
             varDeclarator.process({
               FunctionExpression: wrap,
               ArrowFunctionExpression: wrap,
-              ObjectExpression: wrap,
-              ArrayExpression: wrap,
             })
           },
         })
