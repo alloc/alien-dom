6a326c5c28d0fe8860f35b21fa74556ad255c35e
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-05-16 18:57:09 -0400

    feat: add `usePrevious` hook

---
+++ src/hooks/usePrevious.ts
@@ -0,0 +1,31 @@
+import { useState } from './useState'
+import { depsHaveChanged } from '../internal/deps'
+import { noop } from '../jsx-dom/util'
+
+export function usePrevious<T>(value: T): T | undefined
+export function usePrevious<T>(value: T, deps: readonly any[]): T | undefined
+export function usePrevious(value: any, deps?: readonly any[]) {
+  const state = useState(initialState, deps)
+
+  const { prev } = state
+  state.prev = value
+
+  if (deps && depsHaveChanged(deps, state.deps!)) {
+    state.deps = deps
+    return undefined
+  }
+  return prev
+}
+
+const initialState = (
+  deps: readonly any[] | undefined
+): {
+  deps: readonly any[] | undefined
+  prev: any
+  dispose: (() => void) | void
+} => ({
+  deps,
+  prev: undefined,
+  // This is defined so HMR knows to clear the useMemo cache.
+  dispose: deps ? noop : undefined,
+})
