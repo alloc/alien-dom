ac103e57f45d45c4f6e4aba9d1eccc61dd2ff380
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-05-17 14:29:52 -0400

    fix: change the return type of `fromElementProp` to reflect the possibly of a comment node

--- src/functions/fromElementProp.ts
+++ src/functions/fromElementProp.ts
@@ -1,12 +1,17 @@
 import type { JSX } from '../types/jsx'
 import { fromElementThunk } from '../internal/fromElementThunk'
 
-type ElementResult = JSX.Element | false | null | undefined
+type ElementResult = JSX.Element | Comment | false | null | undefined
 
 /**
  * Coerce a possible element thunk into an element (or a falsy value),
  * while ensuring the thunk isn't executed more than once in its
  * lifetime.
+ *
+ * ⚠️ This can return a `Comment` node if the given JSX element was
+ * declared as a fragment (since every JSX fragment's first child is a
+ * comment node) or a composite element (but only if its component
+ * returns null or it threw an error during a hot update).
  */
 export function fromElementProp(element: JSX.ElementProp): ElementResult
 
