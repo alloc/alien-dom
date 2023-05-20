0745cce62a038787e4ac2a9e121fe92d1efa4a1c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-05-20 10:35:44 -0400

    fix(types): change `toElements` to return an array of DefaultElement objects

--- src/functions/toElements.ts
+++ src/functions/toElements.ts
@@ -1,19 +1,21 @@
 import type { JSX } from '../types'
-import type { AnyElement } from '../internal/types'
+import type { DefaultElement } from '../internal/types'
 import { kAlienPlaceholder } from '../internal/symbols'
 import { toChildNodes } from '../internal/fragment'
 import { isFragment, isElement } from '../internal/duck'
 
-export function toElements(element: JSX.ElementOption): AnyElement[] {
+export function toElements(element: JSX.ElementOption): DefaultElement[] {
   if (!element) {
     return []
   }
   if (isFragment(element)) {
-    const childElements: AnyElement[] = []
+    const childElements: DefaultElement[] = []
     const childNodes = toChildNodes(element as any)
     childNodes.forEach(child => {
       if (isElement(child)) {
-        childElements.push(kAlienPlaceholder(child) || (child as AnyElement))
+        childElements.push(
+          kAlienPlaceholder(child) || (child as DefaultElement)
+        )
       }
     })
     return childElements
