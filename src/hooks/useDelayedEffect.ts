f26342c4eb359d129fb5e1d6d366c6c7eb209304
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-07-02 15:28:49 -0400

    feat: allow multiple numbers/promises as delays in `useDelayedEffect`

--- src/hooks/useDelayedEffect.ts
+++ src/hooks/useDelayedEffect.ts
@@ -1,4 +1,9 @@
+import { isFunction, isNumber } from '@alloc/is'
+import { toArray } from '../jsx-dom/util'
+import { DisposablePromise } from '../promises'
 import { EffectCallback, useEffect } from './useEffect'
+
+type VarArgs<T> = T | readonly T[]
 
 /**
  * Set a timeout to run an effect after a delay, with proper cleanup on unmount.
@@ -7,29 +12,43 @@ import { EffectCallback, useEffect } from './useEffect'
  * argument value is changed in a future render).
  */
 export function useDelayedEffect(
-  delay: number,
+  delay: VarArgs<number | DisposablePromise<any>>,
   effect: EffectCallback,
   deps: readonly any[] = []
 ) {
   useEffect(() => {
     let result: ReturnType<EffectCallback>
+    let numPending = 0
 
-    const timerId = setTimeout(() => {
-      try {
-        result = effect()
-      } catch (error) {
-        console.error(error)
+    const finishOne = () => {
+      if (--numPending == 0) {
+        try {
+          result = effect()
+        } catch (error) {
+          console.error(error)
+        }
       }
-    }, delay)
+    }
+
+    const disposers = toArray(delay).map(delay => {
+      numPending++
+      if (isNumber(delay)) {
+        const timerId = setTimeout(finishOne, delay)
+        return () => clearTimeout(timerId)
+      }
+      delay.then(finishOne)
+      return () => delay.dispose()
+    })
 
     return () => {
-      clearTimeout(timerId)
-      if (typeof result == 'function') {
+      if (isFunction(result)) {
         try {
           result()
         } catch (error) {
           console.error(error)
         }
+      } else {
+        disposers.forEach(dispose => dispose())
       }
     }
   }, deps)
