52aa9fbeadcc5e193fd8284173f58990424bc5e0
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-07-07 11:18:57 -0400

    feat: export `useElementRef` hook
    It returns a proxy that forwards access to the underlying DOM element. Pass the proxy as `ref` prop of a JSX element.

---
+++ src/hooks/useElementRef.ts
@@ -0,0 +1,32 @@
+import { useState } from './useState'
+
+export type ElementRef<T extends Element> = T & {
+  setElement(element: T): void
+  toElement(): T | null
+}
+
+export function useElementRef<T extends Element>() {
+  return useState(initElementRef<T>)
+}
+
+const initElementRef = <T extends Element>(): ElementRef<T> => {
+  let element: T | null = null
+  return new Proxy(
+    {
+      toElement() {
+        return element
+      },
+      setElement(newElement: T) {
+        element = newElement
+      },
+    } as ElementRef<T>,
+    {
+      get(target, prop) {
+        if (element && prop in element) {
+          return (element as any)[prop]
+        }
+        return (target as any)[prop]
+      },
+    }
+  )
+}
