cb33ac65219a58807ca6636a286b330d3348e610
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-07-10 17:22:42 -0400

    fix: correctly handle nested refs in observable `class` prop
    …and ensure class updates are always applied to the correct node.

--- src/functions/classToString.ts
+++ src/functions/classToString.ts
@@ -1,13 +1,16 @@
 import { isArray, isBoolean, isObject } from '@alloc/is'
+import { Disposable } from '../disposable'
 import { DefaultElement } from '../internal/types'
 import { enablePropObserver } from '../jsx-dom/jsx-runtime'
 import { keys } from '../jsx-dom/util'
-import { isRef } from '../observable'
+import { Ref, isRef } from '../observable'
 import { DOMClassAttribute } from '../types'
 
 export function classToString(
-  value: DOMClassAttribute,
+  value: DOMClassAttribute | Ref<DOMClassAttribute>,
   node?: DefaultElement,
+  refs = new Set<Ref>(),
+  observers = new Map<Ref, Disposable>(),
   rootValue?: DOMClassAttribute
 ): string {
   let result: string | undefined
@@ -15,22 +18,32 @@ export function classToString(
   if (value != null && !isBoolean(value)) {
     if (isArray(value)) {
       for (let i = 0; i < value.length; i++) {
-        const names = classToString(value[i], node, rootValue ?? value)
+        const names = classToString(
+          value[i],
+          node,
+          refs,
+          observers,
+          rootValue ?? value
+        )
         if (names) {
           result = (result ? result + ' ' : '') + names
         }
       }
     } else if (isRef(value)) {
-      result = classToString(value.peek(), node)
+      result = classToString(value.peek(), node, refs, observers, rootValue)
       if (node) {
-        enablePropObserver(node, 'class', value, () => {
-          // Update the class attribute without recreating the observer.
-          node.setAttribute('class', classToString(rootValue))
-        })
+        enableClassObserver(value, node, refs, observers, rootValue)
       }
     } else if (isObject(value) && value.constructor !== DOMTokenList) {
       for (const name of keys(value)) {
-        if ((value as any)[name]) {
+        let enabled = (value as any)[name] as boolean | Ref<boolean>
+        if (isRef(enabled)) {
+          if (node) {
+            enableClassObserver(enabled, node, refs, observers, rootValue)
+          }
+          enabled = enabled.peek()
+        }
+        if (enabled) {
           result = (result ? result + ' ' : '') + name
         }
       }
@@ -40,4 +53,30 @@ export function classToString(
   }
 
   return result ?? ''
+}
+
+function enableClassObserver(
+  ref: Ref,
+  node: DefaultElement,
+  refs: Set<Ref>,
+  observers: Map<Ref, Disposable>,
+  rootValue: DOMClassAttribute
+) {
+  refs.add(ref)
+  if (!observers.has(ref)) {
+    observers.set(
+      ref,
+      enablePropObserver(node, 'class', ref, node => {
+        const refs = new Set<Ref>()
+        node.setAttribute(
+          'class',
+          classToString(rootValue, node, refs, observers)
+        )
+        // Dispose observers of nested refs that are no longer used.
+        observers.forEach(
+          (observer, ref) => refs.has(ref) || observer.dispose()
+        )
+      })
+    )
+  }
 }
