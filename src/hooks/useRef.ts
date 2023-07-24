75933577506d8c4d5b378309feae26f9d1a96118
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-07-24 18:07:19 -0400

    feat(types): allow `useRef` to receive no argument

--- src/hooks/useRef.ts
+++ src/hooks/useRef.ts
@@ -1,12 +1,17 @@
 import { currentComponent } from '../internal/global'
 import { Ref, ref } from '../observable'
 
+export function useRef<T>(): Ref<T | undefined> &
+  [value: T | undefined, set: Ref<T | undefined>[1]]
+
 export function useRef<T>(
   init: T | (() => T)
-): Ref<T> & [value: T, set: Ref<T>[1]] {
+): Ref<T> & [value: T, set: Ref<T>[1]]
+
+export function useRef<T>(init?: T | (() => T)): Ref<any> {
   const component = currentComponent.get()!
   const index = component.nextHookIndex++
   return (component.hooks[index] ||= ref(
     init instanceof Function ? init() : init
-  )) as any
+  ))
 }
