11587a8aa36f1aad454db6ea3637ad25d05e18a4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2023-07-31 13:20:17 -0400

    feat(useRef): support dependency array

--- src/hooks/useRef.ts
+++ src/hooks/useRef.ts
@@ -1,5 +1,6 @@
 import { currentComponent } from '../internal/global'
 import { Ref, ref } from '../observable'
+import { useMemo } from './useMemo'
 
 export function useRef<T>(): Ref<T | undefined> &
   [value: T | undefined, set: Ref<T | undefined>[1]]
@@ -8,9 +9,22 @@ export function useRef<T>(
   init: T | (() => T)
 ): Ref<T> & [value: T, set: Ref<T>[1]]
 
-export function useRef<T>(init?: T | (() => T)): Ref<any> {
+export function useRef<T>(
+  init: T | (() => T),
+  deps: readonly any[]
+): Ref<T> & [value: T, set: Ref<T>[1]]
+
+export function useRef<T>(
+  init?: T | (() => T),
+  deps?: readonly any[]
+): Ref<any> {
   const component = currentComponent.get()!
   const index = component.nextHookIndex++
+  if (deps) {
+    useMemo(() => {
+      component.hooks[index] = null
+    }, deps)
+  }
   return (component.hooks[index] ||= ref(
     init instanceof Function ? init() : init
   ))
