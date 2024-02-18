c80d3d05135ca08695f8ede24406aff61ed54d1b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-02-18 01:51:14 -0500

    fix(useObserver): update `onChange` callback transparently
    When a ref is passed as the first argument, the onChange callback can be updated transparently (i.e. without a deps array), since the references within the callback are never observed.
    
    Apart from a better DX (not having to manually define a deps array), the other motivation was to fix a bug where the onChange callback was being called unnecessarily when the deps changed.

--- src/hooks/useObserver.ts
+++ src/hooks/useObserver.ts
@@ -1,14 +1,14 @@
 import { isFunction } from '@alloc/is'
 import { Falsy } from '@alloc/types'
 import { ReadonlyRef, observe } from '../observable'
+import { useCallbackProp } from './useCallbackProp'
 import { EffectCallback, useEffect } from './useEffect'
 import { useHookOffset } from './useHookOffset'
 
 /** Observe a single ref. */
 export function useObserver<T>(
   ref: ReadonlyRef<T> | Falsy,
-  onChange: (value: T, oldValue: T) => void,
-  deps: readonly any[]
+  onChange: (value: T, oldValue: T) => void
 ): void
 
 /** Observe any refs accessed by the effect. */
@@ -24,19 +24,16 @@ export function useObserver(
     const effect = arg1,
       deps = arg2 as readonly any[]
     useEffect(() => observe(effect).destructor, deps)
+  } else if (arg1) {
+    const ref = arg1 as ReadonlyRef<any>,
+      onChange = useCallbackProp(arg2 as (value: any, oldValue: any) => void)
+    useEffect(() => {
+      const initialValue = ref.value
+      onChange(initialValue, initialValue)
+      return observe(ref, onChange).destructor
+    }, [ref])
   } else {
-    const ref = arg1,
-      onChange = arg2 as (value: any, oldValue: any) => void,
-      deps = arg3!
-
-    if (ref) {
-      useEffect(() => {
-        const initialValue = ref.value
-        onChange(initialValue, initialValue)
-        return observe(ref, onChange).destructor
-      }, deps)
-    } else {
-      useHookOffset(2)
-    }
+    // This only handles the first overload: observing a single ref.
+    useHookOffset(3)
   }
 }
