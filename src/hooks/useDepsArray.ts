851e0645b4982df8869b4d57ae3ab0da5ab5749a
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-02-22 22:56:17 -0500

    fix: useRef deps param

--- src/hooks/useDepsArray.ts
+++ src/hooks/useDepsArray.ts
@@ -1,13 +1,15 @@
 import { depsHaveChanged } from '../functions/depsHaveChanged'
 import { currentComponent } from '../internal/global'
 
-export function useDepsArray(deps: readonly any[]) {
+export function useDepsArray(deps: readonly any[] | undefined) {
   const component = currentComponent.get()!
   const index = component.nextHookIndex++
-  const lastDeps = component.hooks[index]
-  if (depsHaveChanged(deps, lastDeps)) {
-    component.hooks[index] = deps
-    return true
+  if (deps) {
+    const lastDeps = component.hooks[index]
+    if (depsHaveChanged(deps, lastDeps)) {
+      component.hooks[index] = deps
+      return true
+    }
   }
   return false
 }
