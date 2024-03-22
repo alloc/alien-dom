f86ec8765990c3afc63bf924fbc9aaf994a44617
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-03-22 14:12:26 -0400

    chore: useEffect description

--- src/hooks/useEffect.ts
+++ src/hooks/useEffect.ts
@@ -5,6 +5,12 @@ import { useState } from './useState'
 export type EffectResult = (() => void) | void
 export type EffectCallback = () => EffectResult
 
+/**
+ * Run an effect after the component is mounted. The effect may rerun on a rerender
+ * if the dependencies have changed. The effect is disposed before the next run.
+ *
+ * 🪝 This hook adds 2 to the hook offset.
+ */
 export function useEffect(effect: EffectCallback, deps: readonly any[]) {
   const state = useState(UseEffect, deps)
   useMicrotask(() => {
