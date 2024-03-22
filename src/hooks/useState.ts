95716a4b240d114606844a5a10fa77a4c8110b02
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-03-21 21:04:26 -0400

    fix(useState): ignore observable access within initializer

--- src/hooks/useState.ts
+++ src/hooks/useState.ts
@@ -1,4 +1,5 @@
 import { expectCurrentComponent } from '../internal/global'
+import { peek } from '../observable'
 
 export function useState<State extends object, Params extends any[]>(
   init: new (...args: Params) => State,
@@ -13,14 +14,16 @@ export function useState<State extends object, Params extends any[]>(
 export function useState(init: StateInitializer, ...params: any[]) {
   const component = expectCurrentComponent()
   const index = component.nextHookIndex++
-  return (component.hooks[index] ||= isClass(init)
-    ? new init(...params)
-    : init(...params))
+  return (component.hooks[index] ||= peek(createState, init, params))
 }
 
 type StateInitializer =
   | ((...params: any[]) => object)
   | (new (...args: any[]) => object)
+
+function createState(init: StateInitializer, params: any[]) {
+  return isClass(init) ? new init(...params) : init(...params)
+}
 
 function isClass(arg: StateInitializer): arg is new (...args: any[]) => any {
   return /^(class[ {]|function [A-Z])/.test(arg.toString())
