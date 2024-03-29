5523dd6a55b25fa23cc0a6bd2e4dee2ed6edd4df
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-03-29 16:38:34 -0400

    feat: add `defineController` and `useController`
    Controllers are used to **control** JSX components from the outside (surprise, surprise).
    
    Controllers are one way. Only the component can access the UI state unless it provides a method that exposes its state. Controllers can replace the UI state, merge into it, or call a method provided by the component.
    
    Controllers can be singletons or uniquely identified instances.

---
+++ src/addons/controller.ts
@@ -0,0 +1,161 @@
+import { useEffect, useSnapshot } from '../hooks'
+import { Refs, createRefs } from '../internal/createRefs'
+
+type CallableProperty<T extends object> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never
+}[keyof T]
+
+export class Controller<State extends object = any, Key = any> {
+  protected instances: Map<any, any> | undefined = undefined
+  constructor(protected singleton = false) {}
+
+  exists(key: Key): boolean {
+    return Boolean(this.instances?.has(key))
+  }
+
+  /**
+   * Set the entire state of an instance. Any properties not provided will be
+   * set to `undefined`.
+   */
+  set(
+    arg1: [Key] extends [void] ? State : Key,
+    arg2: [Key] extends [void] ? void : State
+  ): void {
+    const key = this.singleton ? void 0 : arg1
+    const instance = this.instances?.get(key)
+    if (!instance) {
+      throw Error(`key ${key} does not exist`)
+    }
+    const values = (this.singleton ? arg1 : arg2) as any
+    for (const prop in instance) {
+      if (!(prop in values)) {
+        instance[prop] = undefined
+      }
+    }
+    for (const [prop, value] of Object.entries(values)) {
+      instance[prop] = value
+    }
+  }
+
+  /**
+   * Patch the state of an instance. Any properties not provided will be left
+   * unchanged.
+   */
+  patch(
+    arg1: [Key] extends [void] ? Partial<State> : Key,
+    arg2: [Key] extends [void] ? void : Partial<State>
+  ): void {
+    const key = this.singleton ? void 0 : arg1
+    const instance = this.instances?.get(key)
+    if (!instance) {
+      throw Error(`key ${key} does not exist`)
+    }
+    Object.assign(instance, this.singleton ? arg1 : arg2)
+  }
+
+  protected call<P extends CallableProperty<State>>(
+    key: Key,
+    method: P,
+    ...args: Parameters<State[P]>
+  ): ReturnType<State[P]> {
+    if (!this.instances?.has(key)) {
+      throw Error(`key ${key} does not exist`)
+    }
+    // Prefer not to throw since the caller can't check if the method actually exists.
+    return this.instances.get(key)[method]?.(...args)
+  }
+}
+
+export type ControllerProxy<State extends object, Key> = unknown &
+  Controller<State, Key> & {
+    [P in CallableProperty<State>]-?: (
+      ...args: Parameters<State[P]>
+    ) => ReturnType<State[P]>
+  }
+
+export function defineController<State extends object>(
+  singleton: true
+): ControllerProxy<State, void>
+
+export function defineController<Key, State extends object>(
+  singleton?: false
+): ControllerProxy<State, Key>
+
+export function defineController(singleton?: boolean) {
+  return new Proxy(new Controller(singleton), {
+    get(ctrl: any, prop: string) {
+      if (ctrl[prop] !== undefined || prop in ctrl) {
+        return ctrl[prop]
+      }
+      // Anything else is a method call.
+      return ctrl.singleton
+        ? ctrl.call.bind(ctrl, void 0, prop)
+        : function (key: any, ...args: any[]) {
+            return ctrl.call(key, prop, ...args)
+          }
+    },
+  })
+}
+
+/**
+ * Only one component can use a controller instance at a time. The `init`
+ * function should declare every possible property, even if the initial value is
+ * `undefined`. Every property in the returned object is observable.
+ */
+export function useController<State extends object, Params extends any[]>(
+  ctrl: Controller<State, void>,
+  init: new (...params: Params) => State,
+  ...params: Params
+): Refs<State>
+
+export function useController<Key, State extends object, Params extends any[]>(
+  ctrl: Controller<State, Key>,
+  key: Key,
+  init: new (...params: Params) => State,
+  ...params: Params
+): Refs<State>
+
+export function useController<State extends object, Params extends any[]>(
+  ctrl: Controller<State, void>,
+  init: (...params: Params) => State,
+  ...params: Params
+): Refs<State>
+
+export function useController<Key, State extends object, Params extends any[]>(
+  ctrl: Controller<State, Key>,
+  key: Key,
+  init: (...params: Params) => State,
+  ...params: Params
+): Refs<State>
+
+export function useController(
+  ctrl: Controller,
+  key?: any,
+  ...params: any[]
+): any {
+  const singleton = ctrl['singleton']
+  const init = singleton ? key : params.shift()
+  key = singleton ? void 0 : key
+
+  const instance = useSnapshot(() => {
+    if (ctrl['instances']?.has(key)) {
+      throw Error(`key ${key} already exists`)
+    }
+    const instance = createRefs(init, params)
+    ctrl['instances'] ||= new Map()
+    ctrl['instances'].set(key, instance)
+    return instance
+  }, [key])
+
+  useEffect(
+    () => () => {
+      const instances = ctrl['instances']!
+      if (instances.delete(key) && !instances.size) {
+        ctrl['instances'] = undefined
+      }
+    },
+    [instance]
+  )
+
+  return instance
+}
