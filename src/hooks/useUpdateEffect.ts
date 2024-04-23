65714e01c10b4609266ba2e889c12764f55e74c4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-04-23 17:36:07 -0400

    feat: add `useUpdateEffect` hook

---
+++ src/hooks/useUpdateEffect.ts
@@ -0,0 +1,20 @@
+import { Falsy } from '@alloc/types'
+import { EffectCallback, useEffect } from './useEffect'
+
+/**
+ * Shorthand for `useEffect` with a `!isFirstRun` check inside the effect.
+ */
+export function useUpdateEffect<State = {}>(
+  effect: EffectCallback<State> | Falsy,
+  deps: readonly any[]
+) {
+  useEffect<State>(
+    effect &&
+      (ctx => {
+        if (!ctx.isFirstRun) {
+          return effect(ctx)
+        }
+      }),
+    deps
+  )
+}
