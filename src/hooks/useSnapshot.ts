1132b4be9895f93e4f1fd041fb3ffc665cb417ac
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-05-18 17:49:39 -0400

    chore: add description to `useSnapshot`

--- src/hooks/useSnapshot.ts
+++ src/hooks/useSnapshot.ts
@@ -3,6 +3,13 @@ import { peek } from '../core/observable'
 import { depsHaveChanged } from '../functions/depsHaveChanged'
 import { useState } from './useState'
 
+/**
+ * Similar to `useMemo` except the callback is wrapped with `peek()` to ensure
+ * observable access is disabled (i.e. not observed by the current component).
+ * This ensures the snapshot's value is only updated when the `deps` change.
+ *
+ * 🪝 This hook adds 1 to the hook offset.
+ */
 export function useSnapshot<T>(
   arg: T | (() => T),
   deps: readonly any[] = []
