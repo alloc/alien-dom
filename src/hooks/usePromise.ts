8deee99d778c2fa3ed0f14edd1ec502a76a42678
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-05-23 16:12:20 -0400

    chore(refactor): call `useSnapshot` in usePromise

--- src/hooks/usePromise.ts
+++ src/hooks/usePromise.ts
@@ -1,20 +1,24 @@
 import { OpenPromise } from '../addons/promises'
-import { useMemo } from './useMemo'
+import { useSnapshot } from './useSnapshot'
 import { useState } from './useState'
 
 const newPromise = <T>() => new OpenPromise<T>()
 
 /**
  * Create an open promise with `useState`-like semantics.
+ *
+ * 🪝 This hook adds 1 to the hook offset.
  */
 export function usePromise<T>(): OpenPromise<T>
 
 /**
- * Create an open promise with `useMemo`-like semantics.
+ * Create an open promise with `useSnapshot`-like semantics.
+ *
+ * 🪝 This hook adds 1 to the hook offset.
  */
 export function usePromise<T>(deps: readonly any[]): OpenPromise<T>
 
 /** @internal */
 export function usePromise<T>(deps?: readonly any[]): OpenPromise<T> {
-  return deps ? useMemo(newPromise<T>, deps) : useState(newPromise<T>)
+  return deps ? useSnapshot(newPromise<T>, deps) : useState(newPromise<T>)
 }
