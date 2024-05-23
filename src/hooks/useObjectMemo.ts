9e0ef48fef332747b736851fa17c5be16ccf0765
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-05-23 17:33:16 -0400

    chore(BREAKING): rename useObjectSnapshot to useObjectMemo

--- src/hooks/useObjectMemo.ts
+++ src/hooks/useObjectMemo.ts
@@ -5,8 +5,8 @@ import { useMemo } from './useMemo'
  * equality is used to determine if a property has changed. Property order does
  * not matter. Undefined values are identical to omitted properties.
  */
-export function useObjectSnapshot<T extends object>(o: T): T
-export function useObjectSnapshot(object: any) {
+export function useObjectMemo<T extends object>(o: T): T
+export function useObjectMemo(object: any) {
   const keys = Object.keys(object)
     .filter(k => object[k] !== undefined)
     .sort()
