50bb02d2b4eaebaeef693b2b75bdac74755e2b6b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-08 14:10:41 -0400

    feat(ElementRef): support `(() => void)` return in `attach` method
    This replaces the AlienEffects return type, which is less intuitive to use than a simple disposer callback.

--- src/addons/elementRef.ts
+++ src/addons/elementRef.ts
@@ -1,12 +1,22 @@
-import { AlienEffects } from '../core/effects'
 import { AnyElement } from '../internal/types'
 import { JSX } from '../types'
 
+/**
+ * An `ElementRef` is attached to a DOM element through a JSX element‘s `ref`
+ * prop. It‘s most often used to enact side effects that are tied to the
+ * lifecycle of the DOM element (i.e. the side effects will be active from the
+ * time the DOM element is mounted to the time it is unmounted).
+ *
+ * Because the JSX `ref` prop accepts multiple “element refs” in the shape of an
+ * array, they can be useful for component hooks that need to attach side
+ * effects to DOM elements, removing the need to hoist your JSX elements just to
+ * pass them into those hooks.
+ */
 export class ElementRef<Element extends AnyElement = AnyElement>
   implements JSX.ElementRef
 {
   readonly element: Element | null = null
-  protected effects: AlienEffects | void = undefined
+  protected dispose: (() => void) | void = undefined
 
   setElement(element: Element | null): void {
     if (this.element) {
@@ -14,17 +24,25 @@ export class ElementRef<Element extends AnyElement = AnyElement>
         return
       }
       this.detach?.(this.element)
-      this.effects?.disable(true)
+      this.dispose?.()
     }
     // @ts-ignore
     this.element = element
     if (element) {
-      this.effects = this.attach?.(element)
-      this.effects?.enable()
+      this.dispose = this.attach?.(element)
     }
   }
 
-  protected attach?(element: Element): AlienEffects | void
+  /**
+   * When this method is called, it means the “element ref” was passed into a JSX
+   * element through its `ref` prop and that JSX element is now mounted to the
+   * document.
+   */
+  protected attach?(element: Element): (() => void) | void
+  /**
+   * When this method is called, it means the DOM element that was attached to
+   * this “element ref” is no longer in the document.
+   */
   protected detach?(element: Element): void
 }
 
@@ -43,7 +61,7 @@ export class DelegatedElementRef<
     super()
   }
 
-  protected attach(element: Element): AlienEffects | void {
+  protected attach(element: Element): (() => void) | void {
     return this.delegate.attach?.(element, this)
   }
 
@@ -55,13 +73,19 @@ export class DelegatedElementRef<
 /**
  * A delegate for `DelegatedElementRef`.
  */
-export interface ElementRefDelegate<Element extends AnyElement, DelegateState> {
+export interface ElementRefDelegate<Element extends AnyElement, Key> {
+  /**
+   * When this method is called, it means the “element ref” was passed into a JSX
+   * element through its `ref` prop and that JSX element is now mounted to the
+   * document.
+   */
   attach?(
     element: Element,
-    ref: DelegatedElementRef<Element, DelegateState>
-  ): AlienEffects | void
-  detach?(
-    element: Element,
-    ref: DelegatedElementRef<Element, DelegateState>
-  ): void
+    ref: DelegatedElementRef<Element, Key>
+  ): (() => void) | void
+  /**
+   * When this method is called, it means the DOM element that was attached to
+   * this “element ref” is no longer in the document.
+   */
+  detach?(element: Element, ref: DelegatedElementRef<Element, Key>): void
 }
