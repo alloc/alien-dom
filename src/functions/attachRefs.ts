f245216c2e20a431d9c0750225eeed3f503c5c53
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-11 17:12:56 -0400

    feat(BREAKING): change purpose of `attachRefs`
    The old function of attachRefs is replaced by `makeObjectObservable`.
    
    The new `attachRefs` is aligned with `attachRef`, but intended for attaching multiple refs at once.

--- src/functions/attachRefs.ts
+++ src/functions/attachRefs.ts
@@ -1,20 +1,53 @@
-import { ref } from '../core/observable'
+import { isWhat } from '@alloc/is'
+import { ReadonlyRef, Unref, isRef } from '../core/observable'
+import { keys } from '../internal/util'
 import { attachRef } from './attachRef'
 
 /**
- * Mutates an object so its enumerable properties are observable.
+ * Attaches refs to an object.
+ *
+ * The `refs` object can override or extend the keys of `object`. The property
+ * values of `refs` must be either an observable ref or undefined.
  */
-export const attachRefs = <Props extends object>(
-  object: Props,
-  didSet?: (key: keyof Props, newValue: any, oldValue: any) => void
-) => {
-  for (const [key, value] of Object.entries(object)) {
+export const attachRefs = <
+  T extends object,
+  R extends object & {
+    [K in keyof T]?: ReadonlyRef<T[K]>
+  }
+>(
+  object: T,
+  refs: R & Record<string, ReadonlyRef<unknown> | undefined>,
+  didSet?: (key: string, newValue: any, oldValue: any) => void
+): AttachRefs<T, R> => {
+  for (const key of keys(refs)) {
+    const ref = refs[key]
+    if (ref === undefined) {
+      // Always assign undefined values so that V8 has less object shapes to
+      // optimize.
+      object[key as keyof T] = undefined!
+      continue
+    }
+    if (DEV && !isRef(ref)) {
+      throw Error(`Expected "${key}" to be a ref, got a ${isWhat(ref)}`)
+    }
     attachRef(
       object,
       key,
-      ref(value),
+      ref as ReadonlyRef,
       didSet as (key: keyof any, newValue: any, oldValue: any) => void
     )
   }
-  return object
+  return object as any
 }
+
+type Id<T> = T
+
+/**
+ * The return type of `attachRefs`.
+ */
+export type AttachRefs<
+  T extends object,
+  R extends Record<string, ReadonlyRef | undefined> & {
+    [K in keyof T]?: ReadonlyRef<T[K]>
+  }
+> = Id<T & { [K in Exclude<keyof R, keyof T>]: Unref<R[K]> }>
