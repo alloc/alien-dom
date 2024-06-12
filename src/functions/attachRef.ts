cc8b17ff04cd1905b3388780391f5a2c8cb19799
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-06-11 21:35:56 -0400

    fix(attachRef): get the `value` descriptor correctly

--- src/functions/attachRef.ts
+++ src/functions/attachRef.ts
@@ -7,7 +7,7 @@ export const attachRef = (
   ref: ReadonlyRef,
   didSet?: (key: keyof any, newValue: any, oldValue: any) => void
 ) => {
-  const { get, set } = Object.getOwnPropertyDescriptor(ref, 'value')!
+  const { get, set } = closestPropertyDescriptor(ref, 'value')!
 
   defineProperty(props, key, {
     configurable: true,
@@ -26,4 +26,15 @@ export const attachRef = (
       : undefined,
   })
   return ref
+}
+
+function closestPropertyDescriptor(obj: object, key: keyof any) {
+  let proto = obj
+  do {
+    const descriptor = Object.getOwnPropertyDescriptor(proto, key)
+    if (descriptor) {
+      return descriptor
+    }
+    proto = Object.getPrototypeOf(proto)
+  } while (proto && proto !== Object.prototype)
 }
